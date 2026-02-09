c940cd7063c960a0c818de5045efe85aea11d9b2
eladsw55 <eladswisa10@gmail.com>
2026-02-09 13:42:29 +0200

    Reorganize imports and comments in database.py

--- database.py
+++ database.py
@@ -1,9 +1,8 @@
-from sqlalchemy import create_all
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# שימוש ב-SQLite מקומי בתוך השרת של Render
+# שימוש ב-SQLite מקומי
 SQLALCHEMY_DATABASE_URL = "sqlite:///./wedding_app.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
